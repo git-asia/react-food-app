0ddd35f572c2737c0d29c0436346ff55bd56c540
git-asia <94705253+git-asia@users.noreply.github.com>
2022-11-21 21:07:45 +0100

    poprawki

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import { useState } from 'react';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
-
 import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+
 
 function App() {
   const  [cartIsShown, setCartIsShown] = useState(false);
